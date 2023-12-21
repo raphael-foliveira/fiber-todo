55fbc5b755f03f9ce9f1334b2fe3016b9d6e8a85
Raphael Oliveira <raphael.foliveira1@gmail.com>
2023-12-21 11:11:23 -0300

    refactor: use pointers when returning from repository functions

--- pkg/todo/repository.go
+++ pkg/todo/repository.go
@@ -7,10 +7,10 @@ import (
 )
 
 type ITodoRepository interface {
-	Create(todo CreateTodoDto) (Todo, error)
+	Create(todo CreateTodoDto) (*Todo, error)
 	List() ([]Todo, error)
-	Retrieve(id int) (Todo, error)
-	Update(todo Todo) (Todo, error)
+	Retrieve(id int) (*Todo, error)
+	Update(todo Todo) (*Todo, error)
 	Delete(id int) (int64, error)
 }
 
@@ -22,16 +22,25 @@ func NewTodoRepository(db *database.Database) *TodoRepository {
 	return &TodoRepository{Db: db}
 }
 
-func (tr *TodoRepository) Create(todo CreateTodoDto) (Todo, error) {
-	row := tr.Db.QueryRow(`
-		INSERT INTO todo (title, description, completed) 
-		VALUES ($1, $2, $3) 
-		RETURNING id, title, description, completed`,
-		todo.Title, todo.Description, todo.Completed,
-	)
+func (tr *TodoRepository) Create(todo CreateTodoDto) (*Todo, error) {
+	tx, err := tr.Db.Begin()
+	if err != nil {
+		return nil, err
+	}
+	stmt, err := tx.Prepare(`
+	INSERT INTO todo 
+		(title, description, completed) 
+	VALUES 
+		($1, $2, $3) 
+	RETURNING id, title, description, completed
+	`)
+	if err != nil {
+		return nil, err
+	}
+	row := stmt.QueryRow(todo.Title, todo.Description, todo.Completed)
 	var createdTodo Todo
-	err := row.Scan(&createdTodo.Id, &createdTodo.Title, &createdTodo.Description, &createdTodo.Completed)
-	return createdTodo, err
+	err = row.Scan(&createdTodo.Id, &createdTodo.Title, &createdTodo.Description, &createdTodo.Completed)
+	return &createdTodo, err
 }
 
 func (tr *TodoRepository) List() ([]Todo, error) {
@@ -52,30 +61,30 @@ func (tr *TodoRepository) List() ([]Todo, error) {
 	return todos, nil
 }
 
-func (tr *TodoRepository) Retrieve(id int) (Todo, error) {
+func (tr *TodoRepository) Retrieve(id int) (*Todo, error) {
 	row := tr.Db.QueryRow("SELECT id, title, description, completed FROM todo WHERE id = $1", id)
 	var todo Todo
 	err := row.Scan(&todo.Id, &todo.Title, &todo.Description, &todo.Completed)
 	if err != nil {
-		return Todo{}, err
+		return nil, err
 	}
-	return todo, nil
+	return &todo, nil
 }
 
-func (tr *TodoRepository) Update(todo Todo) (Todo, error) {
+func (tr *TodoRepository) Update(todo Todo) (*Todo, error) {
 	result, err := tr.Db.Exec("UPDATE todo SET title = $1, description = $2, completed = $3 WHERE id = $4",
 		todo.Title, todo.Description, todo.Completed, todo.Id)
 	if err != nil {
-		return Todo{}, err
+		return nil, err
 	}
 	rowsAffected, err := result.RowsAffected()
 	if err != nil {
-		return Todo{}, err
+		return nil, err
 	}
 	if rowsAffected == 0 {
-		return Todo{}, errors.New("todo not found")
+		return nil, errors.New("todo not found")
 	}
-	return todo, nil
+	return &todo, nil
 }
 
 func (tr *TodoRepository) Delete(id int) (int64, error) {
